3aade92be527c11e654e7125adddcca8e656b09f
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2019-12-13 20:55:47 -0500

    Starting navigation style

--- src/Navigation/LargeSidebar.js
+++ src/Navigation/LargeSidebar.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function LargeSidebar() {
     return (
-        <div className="largeSidebar"></div>
+        <div className="largeSidebar">
+            <div className="largeSidebar__work">Work</div>
+            <div className="largeSidebar__about">About</div>
+            <div className="largeSidebar__gear">Gear</div>
+            <div className="largeSidebar__contact">Contact</div>
+        </div>
     )
 }
