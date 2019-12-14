00a77c53fb55e2848f4e5d59e2b8ed48ab8f7651
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2019-12-14 11:37:28 -0500

    Small Sidebar Finished

--- src/Navigation/SmallSidebar.js
+++ src/Navigation/SmallSidebar.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+// SVGs
+import { ReactComponent as WorkIcon } from '../assets/navigation/film.svg';
+import { ReactComponent as AboutIcon } from '../assets/navigation/info.svg';
+import { ReactComponent as GearIcon } from '../assets/navigation/video-camera.svg';
+import { ReactComponent as ContactIcon } from '../assets/navigation/phone.svg';
 
 export default function SmallSidebar() {
     return (
         <div className="smallSidebar">
-            <div className="smallSidebar__work">Work</div>
-            <div className="smallSidebar__about">About</div>
-            <div className="smallSidebar__gear">Gear</div>
-            <div className="smallSidebar__contact">Contact</div>
+            <div className="smallSidebar__work">
+                <WorkIcon className="smallSidebar__work-icon"/>
+            </div>
+            <div className="smallSidebar__about">
+                <AboutIcon className="smallSidebar__about-icon"/>
+            </div>
+            <div className="smallSidebar__gear">
+                <GearIcon className="smallSidebar__gear-icon"/>
+            </div>
+            <div className="smallSidebar__contact">
+                <ContactIcon className="smallSidebar__contact-icon"/>
+            </div>
         </div>
     )
 }
