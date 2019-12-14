1c284f2b267cb92d17f2710127c4472ff1724bfa
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2019-12-14 15:21:59 -0500

    Styling Large Sidebar

--- src/Navigation/LargeSidebar.js
+++ src/Navigation/LargeSidebar.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
+
+// SVGs
+import { ReactComponent as WorkIcon } from '../assets/navigation/film.svg';
+import { ReactComponent as AboutIcon } from '../assets/navigation/info.svg';
+import { ReactComponent as GearIcon } from '../assets/navigation/video-camera.svg';
+import { ReactComponent as ContactIcon } from '../assets/navigation/phone.svg';
 
 export default function LargeSidebar() {
     return (
         <div className="largeSidebar">
-            <div className="largeSidebar__work">Work</div>
-            <div className="largeSidebar__about">About</div>
-            <div className="largeSidebar__gear">Gear</div>
-            <div className="largeSidebar__contact">Contact</div>
+            <div className="largeSidebar__work">
+                <p className="largeSidebar__work-text">Work</p>
+                <WorkIcon className="largeSidebar__work-icon" />
+            </div>
+            <div className="largeSidebar__about">
+                <p className="largeSidebar__about-text">About</p>
+                <AboutIcon className="largeSidebar__about-icon" />
+            </div>
+            <div className="largeSidebar__gear">
+                <p className="largeSidebar__gear-text">Gear</p>
+                <GearIcon className="largeSidebar__gear-icon" />
+            </div>
+            <div className="largeSidebar__contact">
+                <p className="largeSidebar__contact-text">Contact</p>
+                <WorkIcon className="largeSidebar__contact-icon" />
+            </div>
         </div>
     )
 }
