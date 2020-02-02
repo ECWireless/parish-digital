73572c2b1d748100b6a2b6dc921dbdfac993dfc8
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-02-02 15:36:18 -0500

    Footer Started

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className="footer">Footer</div>
+        <div className="footer">
+            <div className="footer__contact">
+                <h3 className="footer__contact-heading">Contact Us</h3>
+                <p className="footer__contact-paragraph">We respond within 24 hours, 7 days a week.</p>
+                <p className="footer__contact-number">(412) 951-3331</p>
+                <div className="footer__contact-social">Social Icons</div>
+            </div>
+            <form action="" className="footer__form">
+                <label htmlFor="" className="footer__form-email-label">Email</label>
+                <input type="text" className="footer__form-email-input"/>
+                <label htmlFor="" className="footer__form-name-label">Name</label>
+                <input type="text" className="footer__form-name-input"/>
+                <label htmlFor="" className="footer__form-subject-label">Subject</label>
+                <input type="text" className="footer__form-subject-input"/>
+                <label htmlFor="" className="footer__form-message-label">Message</label>
+                <textarea name="" className="footer__form-message-input"></textarea>
+            </form>
+            <div className="footer__bottom">
+
+            </div>
+        </div>
     )
 }
