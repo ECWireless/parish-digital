013f5f1cdef0c22c62023e2c757f418902590a90
Elliott Conway <elliottconway@Elliotts-MBP.fios-router.home>
2020-02-18 11:55:15 -0500

    H1 Started

--- src/Pages/HomePage/H1.js
+++ src/Pages/HomePage/H1.js
@@ -3,10 +3,15 @@ import webVideo from '../../assets/home-page/hero-video-main.mp4';
 
 export default function H1() {
     return (
-        <div className="h1__web-hero">
+        <div className="h1">
             <div className="h1__web-hero-container">
                 <video src={webVideo} className="h1__web-hero-video" autoPlay loop muted playsInline />
             </div>
+            <div className="h1__overlay" />
+
+            <h1 className="h1__slogan">Your Partner in Video</h1>
+            <div className="h1__line" />
+            <h2 className="h1__sub-slogan">Our focus is video for business</h2>
         </div>
     )
 }
