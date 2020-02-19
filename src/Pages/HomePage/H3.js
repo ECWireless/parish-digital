3d8c5d310afe1cb6ef15e129bd7e2ec59993eed9
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-02-18 21:25:43 -0500

    H3 Finished

--- src/Pages/HomePage/H3.js
+++ src/Pages/HomePage/H3.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+// Photo
+import Photo from '../../assets/home-page/mike.png'
 
 export default function H3() {
     return (
-        <div className="h3">Parish Digital</div>
+        <div className="h3">
+            <img className="h3__photo" src={Photo} alt=""/>
+            <h2 className="h3__heading">Parish Digital</h2>
+            <p className="h3__paragraph">Since 2007, we've consistently produced top-notch videos and happy clients.</p>
+            <button className="h3__button">Located in Pittsburgh, PA</button>
+            <div className="h3__line" />
+        </div>
     )
 }
