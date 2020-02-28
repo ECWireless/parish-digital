a9f9d07b457e67a6e45125e88ebac7b40d77dd23
Elliott Conway <elliottconway@Elliotts-MacBook-Pro.local>
2020-02-28 16:03:13 -0500

    Started changes to Home Page

--- src/Pages/HomePage/H2.js
+++ src/Pages/HomePage/H2.js
@@ -3,18 +3,31 @@ import React from 'react'
 export default function H2() {
     return (
         <div className="h2">
-            <h2 className="h2__heading">What makes us a great partner in video:</h2>
-            <div className="h2__line h2__line--1" />
-            <div className="h2__line h2__line--2" />
-
-            <h3 className="h2__sub-headings h2__sub-headings--1">We're Reliable</h3>
-            <p className="h2__paragraphs h2__paragraphs--1">We do what we say and we do it on time.</p>
-            <h3 className="h2__sub-headings h2__sub-headings--2">We're Helpful</h3>
-            <p className="h2__paragraphs h2__paragraphs--2">We listen. When you have an issue, we've go you covered.</p>
-            <h3 className="h2__sub-headings h2__sub-headings--3">We're Committed</h3>
-            <p className="h2__paragraphs h2__paragraphs--3">We'll do what's necessary to bring your vision to life.</p>
-            <h3 className="h2__sub-headings h2__sub-headings--4">We're Respectful</h3>
-            <p className="h2__paragraphs h2__paragraphs--4">We keep our ego at the door and keep your interests at heart.</p>
+            <div className="h2__background" />
+            <h2 className="h2__heading">What makes us a great partner in video</h2>
+            <div className="h2__texture h2__texture--1" />
+            <div className="h2__texture h2__texture--2" />
+            <div className="h2__features-container h2__features-container--1">
+                <div className="h2__overlay" />
+                <h3 className="h2__sub-heading">We're Reliable</h3>
+                <p className="h2__paragraph">We do what we say and we do it on time</p>
+            </div>
+            <div className="h2__features-container h2__features-container--2">
+                <div className="h2__overlay" />
+                <h3 className="h2__sub-heading">We're Committed</h3>
+                <p className="h2__paragraph">We'll do what's necessary to bring your vision to life</p>
+            </div>
+            <div className="h2__features-container h2__features-container--3">
+                <div className="h2__overlay" />
+                <h3 className="h2__sub-heading">We're Respectful</h3>
+                <p className="h2__paragraph">We keep our ego at the door and keep your interests at heart</p>
+            </div>
+            <div className="h2__features-container h2__features-container--4">
+                <div className="h2__overlay" />
+                <h3 className="h2__sub-heading">We're Helpful</h3>
+                <p className="h2__paragraph">We listen. When you have an issue, we've got you covered</p>
+            </div>
+            
         </div>
     )
 }
--- src/Pages/HomePage/H3.js
+++ src/Pages/HomePage/H3.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
-// Photo
-import Photo from '../../assets/home-page/mike.png'
-
 export default function H3() {
     return (
         <div className="h3">
-            <img className="h3__photo" src={Photo} alt=""/>
+            {/* <img className="h3__photo" src={Photo} alt=""/>
             <h2 className="h3__heading">Parish Digital</h2>
             <p className="h3__paragraph">Since 2007, we've consistently produced top-notch videos and happy clients.</p>
             <button className="h3__button">Located in Pittsburgh, PA</button>
-            <div className="h3__line" />
+            <div className="h3__line" /> */}
+
+            <div className="h3__background-1" />
+            <div className="h3__background-2" />
+            <div className="h3__background-3" />
+
+            <div className="h3__section-1">
+                <div className="h3__photo-background" />
+                <div className="h3__photo" />
+                <h2 className="h3__heading">What we do</h2>
+                <div className="h3__heading-bar" />
+                <div className="h3__heading1-line" />
+            </div>
+
+            <div className="h3__section-2">
+
+            </div>
+
+            <div className="h3__section-3">
+
+            </div>
         </div>
     )
 }
