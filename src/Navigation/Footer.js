ac60afdbd2d1223ea6546eda93ede3e9c8bace20
ECWireless <Elliott@valtechcreative.com>
2020-08-04 09:08:42 -0600

    Change Footer Badge link

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -6,10 +6,9 @@ export default function Footer() {
             <h2 className="footer__heading">Contact Us</h2>
             <p className="footer__paragraph">We respond within 24 hours, 7 days a week.</p>
             <a href="tel:412-951-3331" className="footer__number">(412) 951-3331</a>
-            <a className="footer__badge-link" href="https://upcity.com/local-marketing-agencies/lists/top-video-production-companies-in-the-united-states">
+            <a className="footer__badge-link" href="https://upcity.com/local-marketing-agencies/profiles/parish-digital-video">
                 <img className="footer__badge" alt="badge" src="https://upcity-marketplace.s3.amazonaws.com/badges/43f09c0f05c5717ea1aa9aa0533ce196.png" />
             </a>
-
         </div>
     )
 }
